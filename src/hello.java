0d78d4a35982e739e1afe217a241abb567d0262b
forente <zerofox16@gmail.com>
2016-11-15 22:12:06 -0600

    random

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 /**
  * Created by richardcamara on 11/6/16.
  */
 public class hello {
 
     public static void main (String[] args){
-        System.out.println("Greetings");
+        Random um = new Random();
+        System.out.println(um.nextInt());
     }
 }
