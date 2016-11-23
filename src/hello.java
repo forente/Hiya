e65a546de746b2cec7b36c5828fcf14df3dae684
forente <zerofox16@gmail.com>
2016-11-23 14:00:11 -0600

    spanish

--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -8,7 +9,10 @@ public class hello {
 
     public static void main (String[] args){
         Random um = new Random();
-        HashMap Hm = new HashMap()
+        HashMap Hm = new HashMap();
         System.out.println(um.nextInt());
+
+        Map<String,String> engSpnDict = new HashMap<String,String>();
+        engSpnDict.put("Monday", "Lunes");
     }
 }
